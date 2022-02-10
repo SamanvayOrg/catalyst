0cb1de6e9a316e0850a4fb7f86f468517328787e
Vivek Singh <petmongrels@gmail.com>
2022-02-10 11:44:29 +0530

    wip

--- server/src/main/java/org/catalysts/commengage/repository/GoogleReverseGeoRepository.java
+++ server/src/main/java/org/catalysts/commengage/repository/GoogleReverseGeoRepository.java
@@ -5,6 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class GoogleReverseGeoRepository {
+    public static final String RESULT_TYPE = "country|administrative_area_level_1|administrative_area_level_2|administrative_area_level_3|administrative_area_level_4|administrative_area_level_5|administrative_area_level_6|administrative_area_level_7|colloquial_area|postal_code|locality";
+
     @Value("${google.api.key}")
     private String apiKey;
+
+    public void getReverseGeocode() {
+
+    }
 }
