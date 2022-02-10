104fd3facd9f447524879834eacc7ac44c35bc5e
Vivek Singh <petmongrels@gmail.com>
2022-02-10 17:35:58 +0530

    parsed google response into map

--- core/src/main/java/org/catalysts/commengage/domain/GoogleReverseGeoResponse.java
+++ core/src/main/java/org/catalysts/commengage/domain/GoogleReverseGeoResponse.java
@@ -1,7 +1,71 @@
 package org.catalysts.commengage.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GoogleReverseGeoResponse {
+    private List<Result> results = new ArrayList<>();
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Result {
+        @JsonProperty("address_components")
+        private List<AddressComponent> addressComponents = new ArrayList<>();
+
+        public List<AddressComponent> getAddressComponents() {
+            return addressComponents;
+        }
+
+        public void setAddressComponents(List<AddressComponent> addressComponents) {
+            this.addressComponents = addressComponents;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class AddressComponent {
+        @JsonProperty("long_name")
+        private String name;
+        private List<String> types = new ArrayList<>();
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public List<String> getTypes() {
+            return types;
+        }
+
+        public void setTypes(List<String> types) {
+            this.types = types;
+        }
+    }
+
+    public Map<String, String> getData() {
+        List<AddressComponent> addressComponents = this.getResults().parallelStream().flatMap(result -> result.addressComponents.parallelStream()).collect(Collectors.toList());
+        HashMap<String, String> data = new HashMap<>();
+        addressComponents.forEach(addressComponent -> {
+            addressComponent.types.stream().filter(type -> !type.equals("political")).forEach(type -> {
+                data.put(type, addressComponent.name);
+            });
+        });
+        return data;
+    }
 }
--- core/src/test/java/org/catalysts/commengage/domain/GoogleReverseGeoResponseTest.java
+++ core/src/test/java/org/catalysts/commengage/domain/GoogleReverseGeoResponseTest.java
@@ -5,10 +5,19 @@ import org.catalysts.commengage.util.FileUtil;
 import org.catalysts.commengage.util.ObjectMapperFactory;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class GoogleReverseGeoResponseTest {
     @Test
     public void deserialize() throws JsonProcessingException {
         String s = FileUtil.readFile("/googleTestResponse.json");
-        ObjectMapperFactory.OBJECT_MAPPER.readValue(s, GoogleReverseGeoResponse.class);
+        GoogleReverseGeoResponse googleReverseGeoResponse = ObjectMapperFactory.OBJECT_MAPPER.readValue(s, GoogleReverseGeoResponse.class);
+        assertNotEquals(0, googleReverseGeoResponse.getResults().size());
+        assertNotEquals(0, googleReverseGeoResponse.getResults().get(0).getAddressComponents().size());
+        Map<String, String> allData = googleReverseGeoResponse.getData();
+        assertEquals("Junia", allData.get("locality"));
+        assertEquals("Ajmer", allData.get("administrative_area_level_2"));
     }
 }
