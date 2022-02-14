ccb3f5ccae1c3d0b12c5104ee0f259b39aefec44
Vivek Singh <petmongrels@gmail.com>
2022-02-14 18:11:48 +0530

    drop tables

---
+++ core/src/main/resources/db/util/delete-all-data.sql
@@ -0,0 +1,7 @@
+drop table if exists flyway_schema_history;
+drop table if exists password_reset_token;
+drop table if exists access_control;
+drop table if exists user_request;
+drop table if exists qr_code;
+drop table if exists users;
+drop table if exists organisation;
